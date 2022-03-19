efd5fb5adef98b6c6cb54d21987bf7584d3b7af6
Joel1930 <fruchtsalat9@gmail.com>
2022-03-19 11:07:12 +0100

    Sharedfolders und mit Database.sh angefangen

---
+++ Database.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#	Datenbank installieren und Konfigurieren
+#
+#Set Root Password
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password Plokijuhz480"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password Plokijuhz480"
+
+# Installation
+sudo apt-get install -y mysql-server
+
+# Open MySQL Port
+sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+#Grant Remote Access but only for 192.168.1.50
+mysql -uroot -Plokijuhz480 <<%EOF%
+	CREATE USER "root"@"192.168.1.55" IDENTIFIED BY "admin";
+	GRANT ALL PRIVILEGES ON *.* TO "root"@"192.168.1.50";
+	FLUSH PRIVILEGES;
+%EOF%
+
+# Restart fuer Konfigurationsaenderung
+sudo service mysql restart
--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,7 @@ Vagrant.configure("2") do |config|
      
     #general vm config
     web.vm.box = "ubuntu/bionic64"
+    web.vm.synced_folder "./sh_web", "/sh_f_web"
 
     
     #Netconfig
@@ -34,6 +35,7 @@ Vagrant.configure("2") do |config|
 
      #general config
     db.vm.box = "ubuntu/bionic64"
+    db.vm.synced_folder "./sh_db", "/s_f_db"
 
     #Netconfig
     db.vm.network "private_network", ip: "192.168.1.55",
@@ -42,11 +44,12 @@ Vagrant.configure("2") do |config|
 
     #Vmspecs DB
     db.vm.provider "virtualbox" do |vb_db|
-      vb_db.name = "DBserverr_M300"
+      vb_db.name = "DBserver_M300"
       vb_db.memory = "2500"
 
 
     end
+    db.vm.provision :shell, path: "./Database.sh"
   end
 end
 
