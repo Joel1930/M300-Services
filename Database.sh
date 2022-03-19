445aaf1240e8ce49e40b3ebc85a9cf1b16d66bac
Joel1930 <fruchtsalat9@gmail.com>
2022-03-19 12:29:15 +0100

    Database.sh changes (SQL)

--- Database.sh
+++ Database.sh
@@ -13,11 +13,17 @@ sudo apt-get install -y mysql-server
 sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 #Grant Remote Access but only for 192.168.1.50
-mysql -uroot -p Plokijuhz480 <<%EOF%
+mysql -uroot -pPlokijuhz480 <<%EOF%
 	CREATE USER "root"@"192.168.1.50" IDENTIFIED BY "admin";
 	GRANT ALL PRIVILEGES ON *.* TO "root"@"192.168.1.50";
 	FLUSH PRIVILEGES;
-	
+
+
+    CREATE DATABASE UserDB;
+	USE UserDB;
+    CREATE TABLE user(userID int auto_increment, name varchar(30), age int, primary key(userID));
+    INSERT INTO user (name, age) VALUES ("Max", 17);
+    
 %EOF%
 
 # Restart fuer Konfigurationsaenderung
