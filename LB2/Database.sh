adafb42724ae6916a7278387a7eba5087344278b
Joel1930 <fruchtsalat9@gmail.com>
2022-03-21 21:40:32 +0100

    Error fixes DB.sh und userinfo.php

---
+++ LB2/Database.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#	Datenbank installieren und Konfigurieren
+#
+#Set Root Password
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password Plokijuhz480"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password Plokijuhz480"
+
+# Installation
+sudo apt-get update
+sudo apt-get install -y mysql-server
+
+# Open MySQL Port
+sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+#Grant Remote Access but only for 192.168.1.50
+mysql -uroot -pPlokijuhz480 <<%EOF%
+	CREATE USER "trj"@"192.168.1.50" IDENTIFIED BY "admin";
+	GRANT ALL PRIVILEGES ON *.* TO "trj"@"192.168.1.50";
+	FLUSH PRIVILEGES;
+
+
+    CREATE DATABASE UserDB;
+	USE UserDB;
+    CREATE TABLE user(userID int auto_increment, name varchar(30), lastname varchar(30), age int, primary key(userID));
+    
+%EOF%
+
+# Restart fuer Konfigurationsaenderung
+sudo service mysql restart
---
+++ LB2/sh_web/userinfo.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>php file - show mysql table</title>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<th>userID</th>
+				<th>name</th>
+				<th>lastname</th>
+				<th>age</th>
+			</tr>
+			<?php
+			if(isset($_POST['submit']))
+				{
+					# save data from from in variables
+					$name = $_POST['name'];
+					$lastname = $_POST['lastname'];
+					$age = $_POST['age'];
+					
+					# database infos
+					$host = "192.168.1.50";
+					$username = "trj";
+					$password = "Plokijuhz480";
+					$dbname = "UserDB";
+
+					# variable for connection
+					$con = mysqli_connect($host, $username, $password, $dbname);
+					
+					# check if connection failed
+					if (!$con)
+					{
+						die("Connection failed!" . mysqli_connect_error());
+					}
+
+					# variable for insert into table
+					$sql = "INSERT INTO user (userID, name, lastname, age) VALUES ('0', '$name', '$lastname', '$age')";
+				
+					# put everything together into mysql query
+					$rs = mysqli_query($con, $sql);
+					if($rs)
+					{
+						echo "Successfully saved";
+					}
+				
+					# SELECT statement in query variable
+					$sqlsel = "SELECT userID, name, lastname, age from benutzer";
+					$result = $con-> query($sqlsel);
+
+					# If rows are more than 0 --> if there is a database
+					if ($result-> num_rows > 0) {
+						while ($row = $result-> fetch_assoc()) {
+							# does the table
+							echo "<br/><br><tr><td>". $row["userID"] ."</td><td>". $row["name"] ."</td><td>". $row["lastname"] ."</td><td>". $row["age"] ."</td></tr>";
+						}
+						echo "</table>";
+					}
+					else {
+						echo "nothing in table";
+					}
+
+					# close mysql connection 
+					mysqli_close($con);
+				}
+			?>
+		</table>
+		<br/>
+		<a href="http://localhost:11930">
+			<input type="submit" value="go back to main"/>
+		</a>		
+	</body>
+</html>
